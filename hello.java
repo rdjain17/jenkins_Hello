ed670b43187e952aa92b74db186d9e63b933bfb4
root <root@ip-172-31-31-127.us-east-2.compute.internal>
2020-02-05 17:26:05 +0000

    Adding hello.java file into jenkins_hello rep in github

---
+++ hello.java
@@ -0,0 +1,13 @@
+class hello 
+{ 
+	public static void main(String args[]) 
+	{
+	       	System.out.println("Hello Rajani"); 
+		System.out.println("How are you?"); 
+		System.out.println("I am doing well and good"); 
+		System.out.println("How about you?"); 
+		System.out.println("Okay fine"); 
+		System.out.println("Bye"); 
+	} 
+}
+
